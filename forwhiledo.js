590403975e6d6a0d61f47fd7d3223526c3f8c0f1
Ostap <drzoiderman12@gmail.com>
2022-11-24 21:32:17 +0200

    цикли

---
+++ forwhiledo.js
@@ -0,0 +1,23 @@
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num)
+//     num++
+   
+// };
+
+// do {
+//     console.log(num)
+//     num++
+// }
+// while (num < 55);
+
+for (let i = 1; i < 9; i++) {
+    if (i === 6) {
+        // break;
+        // continue; пропускає значення задане в умові
+    }
+    
+    console.log(i)
+    
+}
